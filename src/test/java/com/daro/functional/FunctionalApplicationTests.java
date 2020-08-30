2a7cda5cb15e920ae90aab04b7c04fab3a78cd97
Daro <mac@Daro-2.local>
2020-08-30 16:21:24 +0100

    add initial commit

---
+++ src/test/java/com/daro/functional/FunctionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daro.functional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
